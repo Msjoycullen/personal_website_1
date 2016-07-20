80abca28a395256c59b682a34b3a1feec21800ab
Msjoycullen <xiaojizhang27@gmail.com>
2016-07-19 21:06:57 -0400

    Changed deploy config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -11,7 +11,7 @@
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'xiaojizhang.com', user: 'joy', roles: %w{web app}, my_property: :my_value
+server '173.255.216.227', user: 'joy', roles: %w{web app}, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
